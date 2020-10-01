ac4253c121e32ff62b6b77138094e14bf0f7f786
bhushanvaidya3 <bhushan.vaidya@gmail.com>
2020-10-01 11:05:08 -0400

    Add project files.

---
+++ BethanysPieShopHRM.App/Services/TokenProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.App.Services
+{
+    public class TokenProvider
+    {
+        public string XsrfToken { get; set; }
+    }
+
+    public class InitialApplicationState
+    {
+        public string XsrfToken { get; set; }
+    }
+}
